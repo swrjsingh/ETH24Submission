39e8733b046108b78a9f195765d3f5bb384bd1a9
Swaraj Singh <72075925+swrjsingh@users.noreply.github.com>
2024-12-05 02:02:54 +0530

    update: set default theme and disable system theme

--- packages/nextjs/components/ThemeProvider.tsx
+++ packages/nextjs/components/ThemeProvider.tsx
@@ -5,5 +5,16 @@ import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { type ThemeProviderProps } from "next-themes/dist/types";
 
 export const ThemeProvider = ({ children, ...props }: ThemeProviderProps) => {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
+  return (
+    <NextThemesProvider
+      attribute="data-theme"
+      defaultTheme="zkonnect"
+      forcedTheme="zkonnect"
+      enableSystem={false}
+      disableTransitionOnChange
+      {...props}
+    >
+      {children}
+    </NextThemesProvider>
+  );
 };
